13352c871a6a03093f9e0049ae5ab9446dcae8da
Bruna Acioli <brunaacioli09@gmail.com>
2026-01-20 13:35:22 -0300

    configuração do cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,13 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "http://localhost:3000",
-  },
+    setupNodeEvents(on, config) {
+      //implement node event listeners here
+    },
+        baseUrl: "http://localhost:3000",
+    projectId: "tissu7",
+    video: true
+  }
 });
+
 
